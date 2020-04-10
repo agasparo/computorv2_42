13dd26c1c3bae279fa3155d9510728730ec350c2
arthur <arthurysiite@gmail.Com>
2020-04-10 17:31:47 +0200

    first push

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"error"
 	"show"
 	"maths_imaginaires"
-
+	"fmt"
 	"strings"
 )
 
@@ -36,38 +36,33 @@ func basic_check(Inputs input.Data, Vars *types.Variable) (int, int, string) {
 	}
 
 	str := strings.Split(strings.Join(Inputs.Input, " "), "=")
-	str[0] = strings.ToLower((strings.Trim(str[0], " "))
+	str[0] = strings.ToLower((strings.Trim(str[0], " ")))
 	str[1] = strings.Trim(str[1], " ")
-	tb1 = strings.Split(str[1], " ")
 
-	if strings.Index(str[0], "var") != -1 {
-		if strings.Index(str[1], "i") != -1 {
-			//maths_imaginaires.GetAll()
-			//nb1, nb2 := maths_imaginaires.ParseOne(str[1])
-			//Vars.Table[str[0]] = &types.Imaginaire{ nb1, nb2 }
-			//t = 0
-		} else if  parser.IsNumeric(str[1]) {
-
-			//val, _ := strconv.ParseFloat(str[1], 64)
-			//Vars.Table[str[0]] = &types.Rationel{ val }
-			//t = 0
-		}
-	}
-
-	if strings.Index(str[0], "mat") != -1 || strings.Index(str[0], "var") != -1 {
+	if strings.Index(str[0], "var") != -1 && strings.Index(str[1], "i") != -1 {
 		
-
-	}
-
-	if strings.Index(str[0], "fun") != -1 {
+		fmt.Println("imaginaires")
+		maths_imaginaires.GetAll(strings.ReplaceAll(str[1], " ", ""))
+		//nb1, nb2 := maths_imaginaires.ParseOne(str[1])
+		//Vars.Table[str[0]] = &types.Imaginaire{ nb1, nb2 }
+		//t = 0
+	} else if strings.Index(str[0], "mat") != -1 || strings.Index(str[0], "var") != -1 {
 		
+		fmt.Println("matrice")
+	} else if  parser.IsNumeric(str[1]) {
 
-	}
+		//val, _ := strconv.ParseFloat(str[1], 64)
+		//Vars.Table[str[0]] = &types.Rationel{ val }
+		//t = 0
+		fmt.Println("rationel")
+	} else if strings.Index(str[0], "fun") != -1 {
+		
+		fmt.Println("function")
+	} else if val, ok := Vars.Table[str[1]]; ok {
 
-	if val, ok := Vars.Table[str[1]]; ok {
-
+		fmt.Println("var")
     	Vars.Table[str[0]] = val
-    	t = 1
+    	//t = 1
     }
 
 	return 1, t, str[0]
--- src/maths_imaginaires/maths_imaginaires.go
+++ src/maths_imaginaires/maths_imaginaires.go
@@ -3,17 +3,49 @@ package maths_imaginaires
 import (
 	"strings"
 	"strconv"
+	"fmt"
 )
 
-func GetAll() {
-	// verifier dans chauqe qu'il ny ai pas de lettrre a par i	
+func GetAll(str string) {
+	
+	signs := ""
+
+	for i := 0; i < len(str); i += 5 {
+
+		if len(str) > i + 4 {
+			fmt.Println(str[i:i + 4])
+			n1, n2 := ParseOne(str[i:i + 4])
+			fmt.Printf("n1 : %f n2 : %f\n", n1, n2)
+		} else {
+			fmt.Println(str[i:len(str)])
+			add := ""
+			if len (str[i:len(str)]) == 1 {
+				add = "+0i"
+			}
+			n1, n2 := ParseOne(str[i:len(str)] + add)
+			fmt.Printf("n1 : %f n2 : %f\n", n1, n2)
+		}
+
+		if i != 0 {
+			signs += string(str[i - 1])
+		}
+	}
+
+	fmt.Println(signs)
+}
+
+func check() {
+
 }
 
 func ParseOne(str string) (x float64, y float64) {
 
 	str_tmp := strings.ReplaceAll(str, "*", "")
 	str_tmp = strings.ReplaceAll(str_tmp, " ", "")
+	str_tmp = strings.ReplaceAll(str_tmp, "-", "+-")
 	new_str := strings.Split(str_tmp, "+")
+
+	fmt.Println(new_str)
 
 	if strings.Index(new_str[0], "i") != -1 {
 		y, _ = strconv.ParseFloat(strings.ReplaceAll(new_str[0], "i", ""), 64)
@@ -24,4 +56,22 @@ func ParseOne(str string) (x float64, y float64) {
 	}
 
 	return x, y
+}
+
+/************************************************************************************************/
+
+func add() {
+
+}
+
+func mul() {
+
+}
+
+func divi() {
+
+}
+
+func sous() {
+
 }
