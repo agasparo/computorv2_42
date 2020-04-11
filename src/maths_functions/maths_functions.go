7d48e9ac6555ab789723f44fcd6429d51fbb64eb
arthur <arthurysiite@gmail.Com>
2020-04-11 19:24:42 +0200

    first push

--- src/maths_functions/maths_functions.go
+++ src/maths_functions/maths_functions.go
@@ -22,7 +22,12 @@ func Init(tab map[int]string, x string, vars *types.Variable) (string) {
 	fmt.Println(x)
 	tab = maths_imaginaires.CalcMulDivi(tab, vars, x)
 	tab = maths_imaginaires.CalcAddSous(tab, vars, x)
+	fmt.Println(CountSign(tab))
+	if CountSign(tab) == 1 {
+		return (JoinTab(tab))
+	}
 	fmt.Println(tab)
+	fmt.Println(CheckSign(tab))
 	return ("ok")
 }
 
@@ -32,4 +37,45 @@ func Getx(str string) (string) {
 	str = str[p1 + 1:len(str)]
 	str = strings.ReplaceAll(str, ")", "")
 	return (str)
+}
+
+func JoinTab(tab map[int]string) (string) {
+
+	str := ""
+
+	for i := 0; i < len(tab); i++ {
+
+		str += tab[i] + " "
+	}
+	return (str)
+}
+
+func CountSign(tab map[int]string) (int) {
+
+	c := 0
+
+	for i := 1; i < len(tab); i += 2 {
+
+		if tab[i] != "" {
+			c++
+		}
+	}
+	return (c)
+}
+
+func CheckSign(tab map[int]string) (int) {
+
+	cmp := "a"
+
+	for i := 1; i < len(tab); i += 2 {
+
+		if cmp == "a" {
+			cmp = tab[i]
+		} else {
+			if cmp != tab[i] && tab[i] != "" {
+				return (0)
+			}
+		}
+	}
+	return (1)
 }
