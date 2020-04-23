f984e90c11911ac580e9e2c0ab9990bc25af6cef
arthur <arthurysiite@gmail.Com>
2020-04-23 13:52:05 +0200

    add readme

--- src/equations/equations.go
+++ src/equations/equations.go
@@ -107,18 +107,77 @@ func Deltainf(Eq Equation, delta float64) (string) {
 
 	deb := (Eq.B * -1)
 	fin := (Eq.A * 2)
-	Rationalx1 := fmt.Sprintf("x1 = (%f + i√%f) / %f\n", deb, Inverse(delta), fin)
-	Rationalx2 := fmt.Sprintf("x2 = (%f - i√%f) / %f", deb, Inverse(delta), fin)
-	return (Rationalx1 + Rationalx2)
+
+	if isFloatInt(Sqrt(Inverse(delta))) {
+
+		i1 := Sqrt(Inverse(delta)) / fin
+		i2 := Sqrt(Inverse(delta)) / fin
+		a1 := deb / fin
+		a2 := deb / fin
+
+		Rationalx1 := fractions.Rational{a1, 0, 0, "", 3, ""}
+		Rationali1 := fractions.Rational{i1, 0, 0, "", 3, ""}
+		fractions.Trasnform(&Rationalx1)
+		fractions.Trasnform(&Rationali1)
+
+		str1 := fmt.Sprintf("x1 = %f + i%f", a1, i1)
+		if len(Rationalx1.Frac) > 0 && len(Rationali1.Frac) > 0 {
+			str1 += fmt.Sprintf("ou x1 = %s + i%s", Rationalx1.Frac, Rationali1.Frac)
+		} else if len(Rationalx1.Frac) > 0 {
+			str1 += fmt.Sprintf("ou x1 = %s + i%f", Rationalx1.Frac, i1)
+		} else if len(Rationali1.Frac) > 0 {
+			str1 += fmt.Sprintf("ou x1 = %f + i%s", a1, Rationali1.Frac)
+		}
+
+		Rationalx2 := fractions.Rational{a2, 0, 0, "", 3, ""}
+		Rationali2 := fractions.Rational{i2, 0, 0, "", 3, ""}
+		fractions.Trasnform(&Rationalx2)
+		fractions.Trasnform(&Rationali2)
+
+		str2 := fmt.Sprintf("\nx2 = %f + i%f", a2, i2)
+		if len(Rationalx2.Frac) > 0 && len(Rationali2.Frac) > 0 {
+			str2 += fmt.Sprintf("ou x2 = %s + i%s", Rationalx2.Frac, Rationali2.Frac)
+		} else if len(Rationalx2.Frac) > 0 {
+			str2 += fmt.Sprintf("ou x2 = %s + i%f", Rationalx2.Frac, i2)
+		} else if len(Rationali2.Frac) > 0 {
+			str2 += fmt.Sprintf("ou x2 = %f + i%s", a2, Rationali2.Frac)
+		}
+		return (str1 + str2)
+	}
+
+	str1 := fmt.Sprintf("x1 = (%f + i√%f) / %f\n", deb, Inverse(delta), fin)
+	str2 := fmt.Sprintf("x2 = (%f - i√%f) / %f", deb, Inverse(delta), fin)
+	return (str1 + str2)
 }
 
 func DeltaSup(Eq Equation, delta float64) (string) {
 
 	deb := (Eq.B * -1)
 	fin := (Eq.A * 2)
-	Rationalx1 := fmt.Sprintf("x1 = (%f + √%f) / %f\n", deb, delta, fin)
-	Rationalx2 := fmt.Sprintf("x2 = (%f - √%f) / %f", deb, delta, fin)
-	return (Rationalx1 + Rationalx2)
+	deb_x1 := deb + Sqrt(delta)
+	deb_x2 := deb - Sqrt(delta)
+	fin_x1 := deb_x1 / fin
+	fin_x2 := deb_x2 / fin
+
+	if isFloatInt(Sqrt(delta)) {
+		Rationalx1 := fractions.Rational{fin_x1, 0, 0, "", 3, ""}
+		fractions.Trasnform(&Rationalx1)
+		str1 := fmt.Sprintf("x1 = %f ", fin_x1)
+		if len(Rationalx1.Frac) > 0 {
+			str1 += fmt.Sprintf("ou x1 = %s", Rationalx1.Frac)
+		}
+		Rationalx2 := fractions.Rational{fin_x2, 0, 0, "", 3, ""}
+		fractions.Trasnform(&Rationalx2)
+		str2 := fmt.Sprintf("\nx2 = %f ", fin_x2)
+		if len(Rationalx2.Frac) > 0 {
+			str2 += fmt.Sprintf("ou x2 = %s", Rationalx2.Frac)
+		}
+		return (str1 + str2)
+	}
+
+	str1 := fmt.Sprintf("x1 = (%f + √%f) / %f\n", deb, delta, fin)
+	str2 := fmt.Sprintf("x2 = (%f - √%f) / %f", deb, delta, fin)
+	return (str1 + str2)
 }
 
 func ResolveDeg1(eq Equation) (int, string) {
@@ -138,4 +197,26 @@ func isFloatInt(floatValue float64) (bool) {
 		return false
 	}
     return floatValue == float64(int64(floatValue))
+}
+
+func Sqrt(x float64) float64 {
+    
+    var z float64 = 1
+
+    for i := 1; i <= 10; i++ {
+
+    	z = (z - (Pow(z, 2) - x) / ( 2 * z))
+    }
+    return (z)
+}
+
+func Pow(x float64, n int) (res float64) {
+
+    number := 1.00;
+
+    for i := 0; i < n; i++ {
+        number *= x;
+    }
+
+    return (number);
 }
