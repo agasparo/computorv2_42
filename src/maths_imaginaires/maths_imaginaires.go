78a68ad1934134d70558b6aba7f712fa01358373
arthur <arthurysiite@gmail.Com>
2020-05-05 14:27:08 +0200

    add readme

--- src/maths_imaginaires/maths_imaginaires.go
+++ src/maths_imaginaires/maths_imaginaires.go
@@ -37,19 +37,18 @@ func CalcMulDivi(data map[int]string, vars *types.Variable, inconnue string) (ma
 		if data[i] == "*" && data[i - 1] != inconnue && data[i + 1] != inconnue && !IsPowFunc(inconnue, data[i - 1], data[i + 1]) {
 			if strings.Index(data[i - 1], "mat") != -1 || strings.Index(data[i + 1], "mat") != -1 {
 				if strings.Index(data[i - 1], "mat") != -1 && strings.Index(data[i + 1], "mat") != -1 {
-					if data[i + 1] == "*" {
-						if !IsOkMul(data[i - 1], data[i + 1], vars) {
-							data[0] = "Your matrice is not good for multiplication"
-							return (data)
-						}
-						Calc = TmpComp{0, 0}
-						Matrices(&Calc, data[i - 1], data[i + 2], "*", vars)
-						data = maps.MapSlice(data, i) // mapsilecount
-						data[i - 1] = data[i - 1]
-					} else {
-						data[0] = "Multiplication with matrices is with ** not just one *"
+					data[0] = "Multiplication with matrices is with ** not just one *"
+					return (data)
+				}
+				if strings.Index(data[i - 1], "mat") != -1 && strings.Index(data[i + 2], "mat") != -1 && data[i + 1] == "*" {
+					if !IsOkMul(data[i - 1], data[i + 2], vars) {
+						data[0] = "Your matrice is not good for multiplication"
 						return (data)
 					}
+					Calc = TmpComp{0, 0}
+					Matrices(&Calc, data[i - 1], data[i + 2], "*", vars)
+					data = maps.MapSlice(data, i) // mapsilecount
+					data[i - 1] = data[i - 1]
 				} else if strings.Index(data[i - 1], "mat") != -1 {
 					nb1, nb2 := ParseOne(data[i + 1], vars)
 					Calc = TmpComp{nb1, nb2}
