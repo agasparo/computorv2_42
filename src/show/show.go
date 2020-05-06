4bf0166c6d923bb8b60883c1389506b5055ba9ea
arthur <arthurysiite@gmail.Com>
2020-05-06 12:58:44 +0200

    add readme

--- src/show/show.go
+++ src/show/show.go
@@ -3,11 +3,19 @@ package show
 import (
 	"types"
 	"fmt"
+	"strings"
 )
 
 func ShowVars(t int, v types.AllT) {
 
 	if t == 0 {
-		fmt.Println(v.Value())
+		res := v.Value()
+		if strings.Index(res, "[") != -1 {
+			res = strings.ReplaceAll(res, ";", "\n")
+			res = res[1:len(res) - 1]
+			fmt.Println(res)
+		} else {
+			fmt.Println(v.Value())
+		}
 	}
 }
