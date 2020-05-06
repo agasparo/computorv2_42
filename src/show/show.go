78bea089a85ccd05750cb8f64a54d4eabbfb2522
arthur <arthurysiite@gmail.Com>
2020-05-06 13:00:23 +0200

    add readme

--- src/show/show.go
+++ src/show/show.go
@@ -12,7 +12,10 @@ func ShowVars(t int, v types.AllT) {
 		res := v.Value()
 		if strings.Index(res, "[") != -1 {
 			res = strings.ReplaceAll(res, ";", "\n")
+			res = strings.ReplaceAll(res, ",", " , ")
 			res = res[1:len(res) - 1]
+			res = strings.ReplaceAll(res, "[", "[ ")
+			res = strings.ReplaceAll(res, "]", " ]")
 			fmt.Println(res)
 		} else {
 			fmt.Println(v.Value())
