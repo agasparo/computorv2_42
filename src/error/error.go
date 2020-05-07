512503afb202642d9a8fdd450d70a567b12fbadb
arthur <arthurysiite@gmail.Com>
2020-05-07 11:58:11 +0200

    add readme

--- src/error/error.go
+++ src/error/error.go
@@ -141,8 +141,11 @@ func In(data map[int]string, t int, f string, Dat types.Variable) (string) {
 							if tab[i] == "" {
 								return ("you have a problem with your parentheses syntaxe")
 							}
-							if strings.Index(tab[i], ",") != -1 {
+							if strings.Index(tab[i], ",") != -1 && (strings.Index(tab[i], "]") == -1 || strings.Index(tab[i], "[") == -1) {
 								return ("'" + tab[i] + "' isn't defined 5")
+							}
+							if strings.Index(tab[i], ",") == -1 {
+								return ("'" + tab[i] + "' isn't defined 6")
 							}
 						}
 					}
